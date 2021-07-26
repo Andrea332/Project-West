53c4723c272ce355b9b0a76fc72cbf155eb3cf77
Andrea332 <andrea.galet3@gmail.com>
2021-07-26 18:38:30 +0200

    implementata l'UI di unity

--- Assets/Assets/Scripts/Input/CursorController.cs
+++ Assets/Assets/Scripts/Input/CursorController.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class CursorController : MonoBehaviour
 {
-    //VARIABILE USATE PER IL SISTEMA MANUALE DI CAMBIO DI INPUT TRA MOUSE/TOUCH E CONTROLLER
-    //public bool usingMouseInput; 
-
+    // Start is called before the first frame update
+    public Text debugClickTesto;
+    public Text debugPosizioneClick;
+    public int numClick;
     public float controllerCursorSpeed;
     public float mouseCursorSpeed;
     private Vector3 mousePosAtControllerSwitch;
     public Vector2 cursorPos;
     public GameObject Cursore;
+    public RectTransform rectTransform;
     private void Start()
     {
         Cursor.visible = false;
@@ -39,22 +42,45 @@ public class CursorController : MonoBehaviour
         */
         /// <summary>
 
-        if (Input.GetAxis("Horizontal") == 0 &&  Input.GetAxis("Vertical") == 0 && Input.mousePosition != mousePosAtControllerSwitch)
+
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0 && Input.mousePosition != mousePosAtControllerSwitch)
         {
             cursorPos = Input.mousePosition * mouseCursorSpeed;
-            mousePosAtControllerSwitch = Input.mousePosition;           
+            mousePosAtControllerSwitch = Input.mousePosition;
         }
         else
         {
             cursorPos += new Vector2(Input.GetAxis("Horizontal") * controllerCursorSpeed, Input.GetAxis("Vertical") * controllerCursorSpeed);
         }
 
-        cursorPos.x = Mathf.Clamp(cursorPos.x, 0, Screen.width - 1);
-        cursorPos.y = Mathf.Clamp(cursorPos.y, 0, Screen.height - 1);
+        cursorPos.x = Mathf.Clamp(cursorPos.x, 0, rectTransform.sizeDelta.x);
+        cursorPos.y = Mathf.Clamp(cursorPos.y, 0, rectTransform.sizeDelta.y);
 
-        Vector3 posizione = Camera.main.ScreenToWorldPoint(cursorPos);
-        posizione = new Vector3(posizione.x, posizione.y, Cursore.transform.position.z);
-        Cursore.transform.position = posizione;
+        /*Vector3 posizione = Camera.main.ScreenToWorldPoint(cursorPos);
+        posizione = new Vector3(posizione.x, posizione.y, 0);*/
+        Cursore.transform.position = cursorPos;
 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            numClick++;
+            debugPosizioneClick.text = "Posizione " + gameObject.transform.position;
+            Raycast();
+        }
+    }
+
+    public void Raycast()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Cursore.transform.position, Vector2.zero, 0);
+
+        if (hit.transform != null)
+        {
+            print(hit.transform.name);
+            debugClickTesto.text = "Cliccato " + hit.transform.name;
+            //print(hit.collider.name);
+            if (hit.collider.TryGetComponent(out Button button))
+            {
+
+            }
+        }
     }
 }
